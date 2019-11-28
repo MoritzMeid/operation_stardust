b79b7c0a77c711e50456d3ccfcc55e368dfc5312
Iris Wie <34252413+Xirai@users.noreply.github.com>
2019-11-28 19:07:22 +0100

    Level 1: Update
    Level 1 - Start from earth into the universe
    - Scrollbackground + Earth + Clouds
    - Download from Asset store

---
+++ Assets/Scripts/MoveClouds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveClouds : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rbc;
+    void Start()
+    {
+        rbc = GetComponent<Rigidbody>();
+        rbc.velocity = transform.up * speed;
+
+    }
+
+
+}
