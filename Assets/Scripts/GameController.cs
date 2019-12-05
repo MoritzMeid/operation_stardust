c54e024aeb0f4705f42dda27673cbe6653b64404
winterlich95 <34250057+winterlich95@users.noreply.github.com>
2019-12-05 15:52:30 +0100

    enemyship/gamecontroller update

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,7 @@ public class GameController : MonoBehaviour
 {
     public GameObject hazard;
     public GameObject hazard_02;
+    public GameObject enemy;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
@@ -56,7 +57,9 @@ public class GameController : MonoBehaviour
                     Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                     Quaternion spawnRotation = Quaternion.identity;
                     Instantiate(hazard, spawnPosition, spawnRotation);
-                    yield return new WaitForSeconds(spawnWait);
+                    Instantiate(hazard_02, spawnPosition, spawnRotation);
+                    Instantiate(enemy, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
 
                 }
             yield return new WaitForSeconds(waveWait);
