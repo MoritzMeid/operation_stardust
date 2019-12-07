626148ae78bb7198eceb34eb63d2587b8e635de4
winterlich95 <34250057+winterlich95@users.noreply.github.com>
2019-12-07 15:15:28 +0100

    Powerupupdate

--- Assets/PowerUp.cs
+++ Assets/PowerUp.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
+public class PowerUp : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
