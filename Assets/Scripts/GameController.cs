8dd9ed4c361ceea58eec6d7c254fbda633e39c53
MoritzMeid <moritzmeid@yahoo.de>
2019-12-10 15:21:33 +0100

    Satelite added + Hazard Array

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,22 +7,26 @@ using TMPro;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
-    public GameObject hazard_02;
-    public GameObject hazard_03;
-    public GameObject enemy;
-    public GameObject enemy_02;
+
+    public GameObject[] hazards;
+    
 
     public Vector3 spawnValues;
+    public Vector3 spawnValuesEnemy;
     public int hazardCount;
     public float spawnWait;
     public float startWait;
     public float waveWait;
+    
     public static float playerHealth;
    // public GUIText scoreText;
     private int score;
     private bool gameOver;
     private bool restart;
+
+    public bool spawnAsteroid;
+    public bool spawnEnemy; 
+
     public int damage;
     public TextMeshProUGUI tmpScore;
     public TextMeshProUGUI healthText;
@@ -83,9 +87,11 @@ public class GameController : MonoBehaviour
         {
             for (int i = 0; i < hazardCount; i++)
             {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
+               
                 yield return new WaitForSeconds(spawnWait);
 
             }
