d6e141dff7c583b1de6c0f3dd4d9114db9c0fcb2
MoritzMeid <moritzmeid@yahoo.de>
2020-01-10 20:18:35 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/MoritzMeid/operation_stardust""
    This reverts commit b28fea6fd2ab92410e5dbc9b3b18033e69d088e5.

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ProgressBar : MonoBehaviour
+{
+
+    private Slider slider;
+    int impWeaponHeat;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = gameObject.GetComponent<Slider>();
+        slider.value = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GameObject player = GameObject.Find("Player");
+        PlayerController playerScript = player.GetComponent<PlayerController>();
+        playerScript.weaponHeat = impWeaponHeat;
+        if(slider.value < impWeaponHeat)
+        {
+            slider.value++;
+        }
+
+    }
+
+}
