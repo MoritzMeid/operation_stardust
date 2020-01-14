d285cf7fa9e4b369008262a6a3d740553c114eb2
winterlich95 <34250057+winterlich95@users.noreply.github.com>
2020-01-14 17:54:05 +0100

    SizePowerup
    Reagenzglas mit dem SizePowerup verknüpft

---
+++ Assets/Scripts/PowerUpS.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpS : MonoBehaviour
+{
+    public float PowerUpTime;
+    public float multiplier = 2;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Size());
+
+
+        }
+    }
+
+    IEnumerator Size()
+    {
+
+        GameObject.FindWithTag("Player").transform.localScale *= multiplier;
+
+        GameObject.FindGameObjectWithTag("PowerUpS").transform.GetChild(0).gameObject.SetActive(false);
+
+ 
+
+        yield return new WaitForSeconds(PowerUpTime);
+
+        GameObject.FindWithTag("Player").transform.localScale /= multiplier;
+
+        Destroy(gameObject);
+
+        yield break;
+    }
+}
