a668d3be0d8ed9b183c0e94b2977f6a4a61ba0ed
Iris Wie <34252413+Xirai@users.noreply.github.com>
2020-01-18 18:42:15 +0100

    Pause Menu
    - Level 4 :heavy_check_mark:

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //        if(GameIsPaused)
+    //        {
+    //            Resume();
+    //        }
+    //        else
+    //        {
+    //            Pause();
+    //        }
+    //    }
+    //}
+
+    public void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+}
