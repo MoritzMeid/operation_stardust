3046f918b8ba130fd8f1918d164ea077d70145c4
Iris Wie <34252413+Xirai@users.noreply.github.com>
2020-01-19 01:11:09 +0100

    Menu buttons undso
    - Level 5 button rausgehauen
    - play = dialogue 1
    :alien:

--- Assets/Prefabs/MainMenu.cs
+++ Assets/Prefabs/MainMenu.cs
@@ -33,20 +33,34 @@ public class MainMenu : MonoBehaviour
         Time.timeScale = 1;
     }
 
-    public void LevelOne()
+    public void LevelDialogueOne()
     {
         SceneManager.LoadScene("dialoguelevel1");
         Time.timeScale = 1f;
-
     }
-    public void LevelTwo()
+    public void LevelDialogueTwo()
     {
         SceneManager.LoadScene("dialoguelevel2");
         Time.timeScale = 1f;
     }
-    public void LevelThree()
+    public void LevelDialogueThree()
     {
         SceneManager.LoadScene("dialoguelevel3");
+        Time.timeScale = 1f;
+    }
+    public void LevelOne()
+    {
+        SceneManager.LoadScene("Level1");
+        Time.timeScale = 1f;
+    }
+    public void LevelTwo()
+    {
+        SceneManager.LoadScene("Level2");
+        Time.timeScale = 1f;
+    }
+    public void LevelThree()
+    {
+        SceneManager.LoadScene("Level3");
         Time.timeScale = 1f;
     }
     public void LevelFour()
